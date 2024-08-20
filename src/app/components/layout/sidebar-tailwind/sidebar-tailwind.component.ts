1ad6af8a12b5fecedf9071243c7f91024da98d2e
iuri.barros <iuri.t.barros@gmail.com>
2024-08-20 09:35:52 -0300

    create tooltip for menu

--- src/app/components/layout/sidebar-tailwind/sidebar-tailwind.component.ts
+++ src/app/components/layout/sidebar-tailwind/sidebar-tailwind.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar-tailwind',
   templateUrl: './sidebar-tailwind.component.html',
-  styleUrl: './sidebar-tailwind.component.css'
+  styleUrl: './sidebar-tailwind.component.scss'
 })
 export class SidebarTailwindComponent {
   isExpanded = true;
