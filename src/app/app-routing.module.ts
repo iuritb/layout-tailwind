ba6dfabb44fd83990a214a14f3096d13eecc450e
iuri.barros <iuri.t.barros@gmail.com>
2024-08-28 09:50:16 -0300

    update routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './views/login/login.component';
+import { CriarSenhaComponent } from './views/criar-senha/criar-senha.component';
+import { VerificaMatriculaComponent } from './views/verifica-matricula/verifica-matricula.component';
+import { ScssMainComponent } from './views/scss-main/scss-main.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'verifica-matricula', pathMatch: 'full' },
+  { path: 'verifica-matricula', component: VerificaMatriculaComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'criar-senha', component: CriarSenhaComponent },
+  { path: 'chat', component: ScssMainComponent },
+  { path: '**', redirectTo: 'verifica-matricula' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
