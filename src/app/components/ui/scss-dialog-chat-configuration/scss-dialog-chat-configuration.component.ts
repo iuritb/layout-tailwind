efc5551cca9e65f1a218f53b9fc78fe7d7dd66cd
iuri.barros <iuri.t.barros@gmail.com>
2024-08-29 09:42:22 -0300

    create chat configuration dialog

--- src/app/components/ui/scss-dialog-chat-configuration/scss-dialog-chat-configuration.component.ts
+++ src/app/components/ui/scss-dialog-chat-configuration/scss-dialog-chat-configuration.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-scss-dialog-chat-configuration',
   templateUrl: './scss-dialog-chat-configuration.component.html',
-  styleUrl: './scss-dialog-chat-configuration.component.css'
+  styleUrl: './scss-dialog-chat-configuration.component.scss'
 })
 export class ScssDialogChatConfigurationComponent {
+  showParamsModal: boolean = false;
+
+  showParams() {
+    this.showParamsModal = true;
+  }
+
+  closeParamsModal() {
+    this.showParamsModal = false;
+  }
+
+  async updateParams() {
+    console.log("configurações de chat atualizadas")
+  }
 
 }
